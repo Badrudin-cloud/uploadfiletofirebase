c0e9ad3ec2b0c86d3be1ca0cd02262aba902849a
hinda <hinda@gmail.com>
2023-01-10 16:51:05 +0300

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDk8JDuoUKKb4LY6VUnt0rzLvhRJ-PmWgM",
-  authDomain: "uploadfile-2c3cc.firebaseapp.com",
-  projectId: "uploadfile-2c3cc",
-  storageBucket: "uploadfile-2c3cc.appspot.com",
-  messagingSenderId: "434977389255",
-  appId: "1:434977389255:web:1d26eda5966ed910295ce2"
+
 };
 
 // Initialize Firebase
